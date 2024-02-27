631f8e274fc8aab7a9352ab556c7cd3b3fbff4e3
Tomek10240 <jindratomm@gmail.com>
2024-02-27 09:10:33 +0100

    nejky vecicky

--- parecek/blog/views.py
+++ parecek/blog/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 def models_list(request):
     random_number = random.randint(a=0, b=100)
-    return HttpResponse(f"Random number{random_number}")
+    return HttpResponse(f"Nahodné čísílko je {random_number}")
