83f74edd7203aeae25bd3b0a690323948e143f3b
Tomek10240 <jindratomm@gmail.com>
2024-03-05 08:08:00 +0100

    nejky vecicky

--- parecek/blog/views.py
+++ parecek/blog/views.py
@@ -5,4 +5,6 @@ from django.http import HttpResponse
 
 def models_list(request):
     random_number = random.randint(a=0, b=100)
-    return HttpResponse(f"Nahodné čísílko je {random_number}")
+    return HttpResponse(f"Vaše supr dupr náhodné čísílko je {random_number}")
+
+#vytvorit html temp do nej nacitat dyn data
